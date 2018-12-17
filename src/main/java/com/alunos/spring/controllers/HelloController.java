8fa3c50f6308ce70ddd101501301caad7fbd51b4
Fernando Fiaux de Moraes <ffiaux@gmail.com>
2018-12-17 21:59:11 -0200

    Alteracoes na controller

--- src/main/java/com/alunos/spring/controllers/HelloController.java
+++ src/main/java/com/alunos/spring/controllers/HelloController.java
@@ -1,7 +1,5 @@
 package com.alunos.spring.controllers;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
@@ -14,28 +12,12 @@ public class HelloController
 	@GetMapping(value = "/")
 	public String index(Locale locale, Model model)
 	{
-		System.out.println("Home Page Requested, locale = " + locale);
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate);		
-		
 		return "index";
 	}	
 	
 	@GetMapping(value = "/hello")
 	public String hello(Locale locale, Model model)
 	{
-		System.out.println("Home Page Requested, locale = " + locale);
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate);		
-		
 		return "hello";
 	}
 }
